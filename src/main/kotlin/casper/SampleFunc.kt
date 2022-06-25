66b92f89d190df3e8ce7b8795328e6242e629e96
hienbui9999 <buiduyhien@gmail.com>
2022-06-25 16:27:09 +0700

    Initial commit

---
+++ src/main/kotlin/casper/SampleFunc.kt
@@ -0,0 +1,10 @@
+package sample_casper_show_information
+class SampleFunc {
+
+    public fun SampleCall() {
+        println("Sample call called")
+    }
+    public fun SampleCall2(a:String) {
+        println("This function call for string:" + a)
+    }
+}
---
+++ src/test/kotlin/casper/SampleFuncTest.kt
@@ -0,0 +1,9 @@
+package casper
+import sample_casper_show_information.SampleFunc
+
+internal class SampleFuncTest {
+    fun test() {
+        val sampleFunc = SampleFunc()
+        sampleFunc.SampleCall2("Hello world, can you tell me")
+    }
+}
